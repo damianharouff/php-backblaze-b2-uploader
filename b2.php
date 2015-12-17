a2d889d479b286fdd4d6137d1b27138a41a77f4a
Xavier Decuyper <xavier.decuyper@gmail.com>
2015-12-17 08:42:22 +0100

    Created central curl method

--- b2.php
+++ b2.php
@@ -1,7 +1,8 @@
 <?php
 
 class B2Uploader{
-	
+
+	// Variables set by user
 	private $bucketId;
 
 	// Authorization stuff
@@ -11,67 +12,37 @@ class B2Uploader{
 	private $authorizationToken;	// Filled in by Authorize() function
 
 	// URLs
+	const AUTHORIZE_URL = 'https://api.backblaze.com/b2api/v1/b2_authorize_account';
 	private $apiUrl;
 	private $downloadUrl;
 	private $uploadUrl;
 
 
 	public function getUploadURL(){
-		$session = curl_init($this->getApiUrl() .  "/b2api/v1/b2_get_upload_url");
+		$url = $this->getApiUrl() .  "/b2api/v1/b2_get_upload_url";
 
-		// Add post fields
-		$data = array("bucketId" => $this->getBucketId());
-		$post_fields = json_encode($data);
-		curl_setopt($session, CURLOPT_POSTFIELDS, $post_fields); 
-
-		// Add headers
+		// Set the headers
 		$headers = array();
 		$headers[] = "Authorization: " . $this->getAuthorizationToken();
-		curl_setopt($session, CURLOPT_HTTPHEADER, $headers); 
 
-		curl_setopt($session, CURLOPT_POST, true); // HTTP POST
-		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);  // Receive server response
-		$server_output = curl_exec($session); // Let's do this!
+		// Post the bucketId fields
+		$postFields = array("bucketId" => $this->getBucketId());
 
-		// Check if we got a HTTP 200 status code
-		if(curl_getinfo($session, CURLINFO_HTTP_CODE) != 200){
-			throw new RuntimeException("Didn't get a 200 response code!");
-		}
-
-		curl_close ($session); // Clean up
-
-		// Decode the JSON
-		$response = json_decode($server_output);
+		// Ask CURL to make the request
+		$response = $this->curlRequest('POST', $url, $headers, $postFields);
 
 		$this->uploadUrl = $response->uploadUrl;
 		$this->authorizationToken = $response->authorizationToken;
-
 	}
 
 	public function authorize(){
-		$url = "https://api.backblaze.com/b2api/v1/b2_authorize_account";
-
-		$session = curl_init($url);
-
-		// Add headers
+		// Set the headers
 		$headers = array();
 		$headers[] = "Accept: application/json";
 		$headers[] = "Authorization: Basic " . $this->getCredentials();
-		curl_setopt($session, CURLOPT_HTTPHEADER, $headers);  // Add headers
-		curl_setopt($session, CURLOPT_HTTPGET, true);  // HTTP GET
-		curl_setopt($session, CURLOPT_RETURNTRANSFER, true); // Receive server response
 
-		$server_output = curl_exec($session);
-
-		// Check if we got a HTTP 200 status code
-		if(curl_getinfo($session, CURLINFO_HTTP_CODE) != 200){
-			throw new RuntimeException("Didn't get a 200 response code!");
-		}
-
-		curl_close ($session);
-
-		// Decode the JSON
-		$response = json_decode($server_output);
+		// Make the request
+		$response = $this->curlRequest('GET', self::AUTHORIZE_URL, $headers);
 
 		// Save the JSON data in our object
 		$this->setApiUrl($response->apiUrl);
@@ -80,15 +51,12 @@ class B2Uploader{
 	}
 
 	public function uploadFile($path){
+		// Open the file for reading
 		$handle = fopen($path, 'r');
 		$read_file = fread($handle,filesize($path));
 
+		// Generate SHA1 hash
 		$sha1_of_file_data = sha1_file($path);
-
-		$session = curl_init($this->uploadUrl);
-
-		// Add read file as post field
-		curl_setopt($session, CURLOPT_POSTFIELDS, $read_file); 
 
 		// Add headers
 		$headers = array();
@@ -96,17 +64,54 @@ class B2Uploader{
 		$headers[] = "X-Bz-File-Name: " . basename($path);
 		$headers[] = "Content-Type: " . 'b2/x-auto';
 		$headers[] = "X-Bz-Content-Sha1: " . $sha1_of_file_data;
-		curl_setopt($session, CURLOPT_HTTPHEADER, $headers); 
 
-		curl_setopt($session, CURLOPT_POST, true); // HTTP POST
-		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);  // Receive server response
-		$server_output = curl_exec($session); // Let's do this!
-		curl_close ($session); // Clean up
-		echo ($server_output); // Tell me about the rabbits, George!
+		// Make the request
+		$response = $this->curlRequest('POST', $this->uploadUrl, $headers, $read_file, true);
+
+		print_r($response);
 	}
 
-	private function curlRequest(){
+	private function curlRequest($httpMethod = 'GET', $url, array $headers = [], $postFields = [], $uploadFile = false){
 
+		$session = curl_init($url);
+
+		// Add the file contents if we're uploading
+		if($uploadFile){
+			curl_setopt($session, CURLOPT_POSTFIELDS, $postFields);
+		}else{
+			// Add post fields
+			if(count($postFields) != 0){
+				$postFields = json_encode($postFields);
+				curl_setopt($session, CURLOPT_POSTFIELDS, $postFields);
+			}
+		}
+
+		// Add headers
+		curl_setopt($session, CURLOPT_HTTPHEADER, $headers);
+
+		// Set HTTP method
+		if($httpMethod === 'GET'){
+			curl_setopt($session, CURLOPT_HTTPGET, true);
+		}else{
+			curl_setopt($session, CURLOPT_POST, true);
+		}
+
+		// Receive server responses
+		curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+		// Fire the request
+		$server_output = curl_exec($session);
+
+		// Check if we got a HTTP 200 status code
+		if(curl_getinfo($session, CURLINFO_HTTP_CODE) != 200){
+			throw new RuntimeException("Didn't get a 200 response code: " . $server_output);
+		}
+
+		// Bye bye CURL!
+		curl_close ($session);
+
+		// Decode & return the JSON
+		return json_decode($server_output);
 	}
 
 	private function getCredentials(){
